ee40b3d2bf4757dc1d96543068589987b5a87c60
sarafontdevila <146539364+sarafontdevila@users.noreply.github.com>
2025-04-14 15:56:59 +0200

    funcion

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,8 +2,27 @@ import "./Header.css"
 import { Home } from '../../pages/Home/Home'
 import { LoginRegister } from '../../pages/LoginRegister/LoginRegister'
 
+const routes = [{
+  texto: "Home",
+  funcion: Home
+},
+{
+  texto: "LoginRegister",
+  funcion: LoginRegister
+}
+]
+
 export const Header =() => { 
   const header = document.querySelector("header")
+  const nav = document.createElement ("nav")
 
-  header.innerHTML = "<h1>Header</h1>"
+  for (const route of routes) {
+    const a = document.createElement("a")
+    a.href = "#"
+    a.textContent = route.texto
+    a.addEventListener("click", route.funcion)
+    nav.append(a)
+  }
+  header.append(nav)
+
 }
--- src/pages/LoginRegister/LoginRegister.js
+++ src/pages/LoginRegister/LoginRegister.js
@@ -1,8 +1,40 @@
+import { Header } from '../../components/Header/Header'
+import { Home } from '../Home/Home'
 import "./LoginRegister.css"
 
 export const LoginRegister =() => {
   const main= document.querySelector("main")
+  main.innerHTML = "";
+  const loginDiv = document.createElement("div")
 
-  app.innerHTML = "<h1>Estamos en Login/register</h1>"
+  Login(loginDiv)
 
+  loginDiv.id = "login"
+
+  main.append(loginDiv)
 }
+
+const Login = (elementoPadre) => { 
+
+  const form = document.createElement("form")
+
+  const inputEmail = document.createElement("input")
+  const inputPassword = document.createElement("input")
+  const button = document.createElement("button")
+
+  inputPassword.type = "password"
+  inputEmail.placeholder = "Email"
+  inputPassword.placeholder = "Contraseña"
+  button.textContent = "Login"
+
+  elementoPadre.append(form)
+  form.append(inputEmail)
+  form.append(inputPassword)
+  form.append(button)
+
+  form.addEventListener("submit", () => submit(inputEmail.value, inputPassword.value, form));
+  }
+
+  const submit = (email, password) => console.log(email, password)
+  
+
