a41411c479244972f59f7bcbdd36abdad9de3a3d
sarafontdevila <146539364+sarafontdevila@users.noreply.github.com>
2025-04-15 18:34:40 +0200

    Home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -9,11 +9,33 @@ export const Home = async() => {
 
   pintarEventos(eventos, main)
 
-  const pintarEventos = (eventos, elementoPadre)
+  const pintarEventos = (eventos, elementoPadre) => {
+    const divEventos = document.createElement("div")
+    divEventos.className = "eventos"
+    for (const evento of eventos) {
+      const divEvento = document.createElement("div")
+      const nombre = document.createElement("h3")
+      const descripcion = document.createElement("p")
+      const fecha = document.createElement("p")
+      const precio = document.createElement("p")
+      const lugar = document.createElement("p")
+      const imagen = document.createElement("img")
+      const asistentes = document.createElement("p")
+
+      titulo.textContent = evento.nombre
+      imagen.src = evento.imagen
+      descripcion.textContent = evento.descripcion
+      fecha.textContent = evento.fecha
+      precio.textContent = "${evento.precio}€"
+      lugar.textContent = evento.lugar
+
+      divEvento.append(titulo, descripcion, fecha, precio, lugar, imagen)
+      divEventos.append(divEvento)
+      elementoPadre.append(divEvento)
+      console.log(evento)
+    }
+  }
 
   
-
-
-
 }
 
